b0cee1e6d8cdbe63471a5a49e3284a8917621e15
MrsYangwy <18273077417@163.com>
2019-10-28 14:14:01 +0800

    interceptor update

--- edu-util/src/main/java/com/yang/edu/util/interceptor/MyWebAppConfigurer.java
+++ edu-util/src/main/java/com/yang/edu/util/interceptor/MyWebAppConfigurer.java
@@ -2,6 +2,7 @@ package com.yang.edu.util.interceptor;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,11 +11,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @Description : 设置过滤
  * @Date : 2019-10-23
  **/
-@SpringBootConfiguration
+@Configuration
 public class MyWebAppConfigurer implements WebMvcConfigurer {
-    /*@Autowired
-    private LoginInterceptor loginInterceptor;*/
-
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoginInterceptor())
